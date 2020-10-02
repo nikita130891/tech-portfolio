9deae49334954c3dc8200eeabf054602ae9e7efc
nikita130891 <nikitajain13@gmail.com>
2020-10-02 12:25:01 -0700

    Update siteConfig.js
    Started my tech-portfolio

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,10 +12,10 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    title: 'Nikita Jain', // Title for your website.
+    tagline: 'Software Development Engineer. Continuously improving, aspiring technologist.',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://nikita130891.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -23,7 +23,7 @@ const siteConfig = {
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'joatmon08',
+    organizationName: 'nikita130891',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
@@ -67,9 +67,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
+        linkedin: 'nikitajain13',
         twitter: '',
-        github: '',
+        github: 'nikita130891',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
